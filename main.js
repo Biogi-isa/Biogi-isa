48d87baabb9c58b976f1bd37f396843a126ddff4
Biogi-isa <Isabelly.vieira.alves01@gmail.com>
2024-09-19 14:04:28 +0000

    arquivo js

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorA11(".botao");
+const textos = document.querySelectorA11(".abas-conteudo")
+
+for(leti = 0 ; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+
+        for(let j = 0;j < botoes. )
+    }
+}
